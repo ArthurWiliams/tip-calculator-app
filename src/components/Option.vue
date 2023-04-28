179f00136318a71de33be534b2a2878174b1521f
Arthur Williams <arthurwiliams86@gmail.com>
2023-04-28 23:32:00 +0300

    Use counter module to generate ids and make focus state apparent

--- src/components/Option.vue
+++ src/components/Option.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import { computed } from "vue";
 
+import Counter from "../counter";
+
 const props = defineProps<{
   modelValue?: string;
   label: string;
   value: string;
   name: string;
 }>();
+
+const OPTION_ID = "option-" + Counter.getCount();
 
 const emits = defineEmits<{
   (e: "update:modelValue", value: string): void;
@@ -27,18 +31,20 @@ function emit(event: Event) {
 </script>
 
 <template>
+  <input
+    class="sr-only peer"
+    type="radio"
+    :name="name"
+    :id="OPTION_ID"
+    :value="value"
+    :checked="IS_SELECTED"
+    @change="emit"
+  />
+
   <label
-    class="px-2 py-[0.6rem] bg w-full block select-none text-center rounded-md cursor-pointer text-2xl active:bg-cyan-300 hover:bg-cyan-300 active:text-cyan-900 hover:text-cyan-900"
+    :for="OPTION_ID"
+    class="peer-focus:bg-cyan-300 peer-focus:text-cyan-900 px-2 py-[0.6rem] bg w-full block select-none text-center rounded-md cursor-pointer text-2xl active:bg-cyan-300 hover:bg-cyan-300 active:text-cyan-900 hover:text-cyan-900"
     :class="SELECTED_STATE"
+    >{{ label }}</label
   >
-    <span>{{ label }}</span>
-    <input
-      class="sr-only"
-      type="radio"
-      :name="name"
-      :value="value"
-      :checked="IS_SELECTED"
-      @change="emit"
-    />
-  </label>
 </template>
